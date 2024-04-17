0525f57cc44cbbb7abbe5e99f1e1065fbca2caf7
shadoud96 <karlos_m96@hotmail.com>
2024-04-17 16:03:12 -0500

    polimorfismo'

--- Screenmatch/src/com/aluracursos/screenmatch/calculos/CalculadoraDeTiempo.java
+++ Screenmatch/src/com/aluracursos/screenmatch/calculos/CalculadoraDeTiempo.java
@@ -1,17 +1,12 @@
 package com.aluracursos.screenmatch.calculos;
 
-import com.aluracursos.screenmatch.modelos.Pelicula;
-import com.aluracursos.screenmatch.modelos.Serie;
+import com.aluracursos.screenmatch.modelos.Titulo;
 
 public class CalculadoraDeTiempo {
     private int tiempoTotal;
 
-    public void incluye(Pelicula pelicula){
-        this.tiempoTotal += pelicula.getDuracionEnMinutos();
-    }
-
-    public void incluye(Serie serie){
-        this.tiempoTotal += serie.getDuracionEnMinutos();
+    public void incluye(Titulo titulo){
+        this.tiempoTotal += titulo.getDuracionEnMinutos();
     }
 
     public int getTiempoTotal() {
